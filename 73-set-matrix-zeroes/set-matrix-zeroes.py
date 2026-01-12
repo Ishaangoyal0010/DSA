26531d24e212f5268c0246f6bfd875663e54a0d0
Ishaan Goyal <143157020+Ishaangoyal0010@users.noreply.github.com>
2026-01-12 19:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (45.11%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,28 +1,17 @@
 class Solution(object):
-    def markinf(self,matrix,row,col):
-        rows=len(matrix)
-        cols=len(matrix[0])
-        for i in range(0,rows):
-            if matrix[i][col]!=0:
-                matrix[i][col]=float('inf')
-        for j in range(0,cols):
-            if matrix[row][j]!=0:
-                matrix[row][j]=float('inf')
-
     def setZeroes(self, matrix):
         rows=len(matrix)
         cols=len(matrix[0])
+        row_track=[0]*rows
+        col_track=[0]*cols
         for i in range(0,rows):
             for j in range(0,cols):
                 if matrix[i][j]==0:
-                    self.markinf(matrix,i,j)
+                    row_track[i]=-1
+                    col_track[j]=-1
 
         for i in range(0,rows):
             for j in range(0,cols):
-                if matrix[i][j]==float('inf'):
+                if row_track[i]==-1 or col_track[j]==-1:
                     matrix[i][j]=0
-        
-
-                    
-
         
