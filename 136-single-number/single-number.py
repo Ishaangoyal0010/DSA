81c878df29488f98ff14565ddbcc10d445ba4f80
Ishaan Goyal <143157020+Ishaangoyal0010@users.noreply.github.com>
2026-01-24 00:31:56 +0530

    Time: 4 ms (52.38%) | Memory: 14 MB (52.00%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,15 +1,6 @@
 class Solution(object):
     def singleNumber(self, nums):
-        n=len(nums)
-        hash_map={}
-        for i in range(0,n):
-            if nums[i] in hash_map:
-                hash_map[nums[i]]+=1
-            else:
-                hash_map[nums[i]]=1
-        for i in hash_map:
-            if hash_map[i]==1:
-                return i
+        return reduce(lambda x, y: x ^ y, nums)
 
                 
                 
