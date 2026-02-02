e2154bcd4a7d574905f7f4d0d9a815ba29f84c45
Ishaan Goyal <143157020+Ishaangoyal0010@users.noreply.github.com>
2026-02-02 14:27:17 +0530

    Time: 21 ms (82.54%) | Memory: 13.1 MB (31.29%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -1,10 +1,10 @@
 class Solution(object):
-    def addToArrayForm(self, num, k):
-        n=len(num)
-        digit=0
-        for i in range(0,n):
-            digit=digit*10+num[i]
-        digit+=k
-        arr=[int(d) for d in str(digit)]
-        return arr
+    def addToArrayForm(self, A, K):
+        A[-1] += K
+        for i in xrange(len(A) - 1, -1, -1):
+            carry, A[i] = divmod(A[i], 10)
+            if i: A[i-1] += carry
+        if carry:
+            A = map(int, str(carry)) + A
+        return A
         
