c1b665562b550036d10c073dcf30bd578804fe6b
Ishaan Goyal <143157020+Ishaangoyal0010@users.noreply.github.com>
2026-02-04 14:59:57 +0530

    Time: 33 ms (56.22%) | Memory: 14 MB (97.84%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        i=0
+        j=0
+        cnt=0
+        while i<len(g) and j<len(s):
+            if g[i]<=s[j]:
+                i+=1
+                j+=1
+                cnt+=1
+            else:
+                j+=1
+            
+        return cnt
