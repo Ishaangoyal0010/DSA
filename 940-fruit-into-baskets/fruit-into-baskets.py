15c07d078a9d95ce125dd885efea7fb47fe34880
Ishaan Goyal <143157020+Ishaangoyal0010@users.noreply.github.com>
2026-02-04 14:21:41 +0530

    Time: 273 ms (29.11%) | Memory: 17.2 MB (54.19%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -3,16 +3,12 @@ class Solution(object):
         left = 0
         maxi = 0
         my_dict = {}
-        
         for right in range(len(fruits)):
-            my_dict[fruits[right]] = my_dict.get(fruits[right], 0) + 1
-            
+            my_dict[fruits[right]] = my_dict.get(fruits[right], 0) + 1 
             while len(my_dict) > 2:
                 my_dict[fruits[left]] -= 1
                 if my_dict[fruits[left]] == 0:
                     del my_dict[fruits[left]]
                 left += 1
-            
             maxi = max(maxi, right-left+1)
-        
         return maxi
